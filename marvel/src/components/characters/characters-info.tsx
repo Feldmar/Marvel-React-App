e60bf121d69dfcd33e8feedadb3c7865d5aa9aea
Khomko Dima <37930691+Khomko-Dima@users.noreply.github.com>
2022-07-25 20:29:16 +0300

    fix get link from id

--- marvel/src/components/characters/characters-info.tsx
+++ marvel/src/components/characters/characters-info.tsx
@@ -1,17 +1,17 @@
 import { Spin } from "antd";
-import { useParams } from "react-router-dom";
-import { CharacterModel } from "src/models/character-model";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useCharacter } from "../hooks/characters-hooks";
 import HeroCard from "./hero-card";
-type TProps = {
-	character : CharacterModel
-}
-const CharactersInfo:FC<TProps> = ({character})=> {
+
+const CharactersInfo = ()=> {
     const {id} = useParams();
+    const navigate = useNavigate()
     const {data, loading} = useCharacter(id);
+    console.log(data);
+    
     const handler = (e: any) => {
 		if (true) {
-			navigate(`${character.id}/comics?`)
+			// navigate(`${character.id}/comics?`)
 		}
 	}
     return (
@@ -43,11 +43,14 @@ const CharactersInfo:FC<TProps> = ({character})=> {
                     ))}
             </div>
             <div onClick={handler}>Comics: {data?.comics.available ? data?.comics.available : 'No comics available for this character.'}
-            {data?.comics.items.map((item,index) => (
-                <li key={index}>
-                    {item.name} ({item.resourceURI})
+            {data?.comics.items.map((item) => {
+                const id = item.resourceURI.split('/').at(-1)
+                return (
+                    <li key={item.name}>
+                        <Link to={'/comics/' + id}>{item.name}</Link>
                     </li>
-                    ))}
+                )
+            })}
             </div>
         </Spin>
     )
